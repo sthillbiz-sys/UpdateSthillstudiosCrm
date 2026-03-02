641615e49a0df38a0480c36c347505f47f153e66
devmarvs <marvin.dev.ph@gmail.com>
2026-03-02 15:31:11 +0100

    remove all current codes

--- login_attempts.sql
+++
@@ -1,9 +0,0 @@
--- Add Google Drive token columns to users table
--- Run this migration to enable Google Drive export feature
-
-ALTER TABLE users 
-ADD COLUMN google_drive_token TEXT NULL,
-ADD COLUMN google_drive_refresh_token TEXT NULL;
-
--- Add index for faster lookups
-CREATE INDEX idx_users_google_drive ON users(id) WHERE google_drive_token IS NOT NULL;
